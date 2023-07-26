96392d65b52d7b6e45bfa1dec077d8f9134a7efa
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-07-26 18:40:59 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+
+    speak = () => {
+        console.log(`${name} says meow!`);
+        return `${name} says meow!`;
+    };
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+
+    speak = () => {
+        console.log(`${name} says woof!`);
+        return `${name} says woof!`;
+    };
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+
+    speak = () => {
+        if (this.sex === 'male') {
+            console.log(`It's me! ${name}, the parrot!`);
+            return `It's me! ${name}, the parrot!`;
+        } else {
+            console.log(`${name} says squawk!`);
+            return `${name} says squawk!`;
+        }
+    };
+}
